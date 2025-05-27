78afdbb26e5be75f696b08657a9e666055b30a12
gadcod <gadrozen5@gmail.com>
2025-05-27 22:20:44 +0000

    final

--- kmeans.c
+++ kmeans.c
@@ -3,6 +3,10 @@
 #include <string.h>
 #include <math.h>
 
+#ifndef INFINITY
+#define INFINITY (1.0/0.0)
+#endif
+
 #define EPSILON 0.001
 
 struct cord {
@@ -15,13 +19,13 @@ struct vector {
     struct cord *cords;
 };
 
-
-// Copying a vector to another vector
+/* Copying a linked list of coordinates */
 struct cord* copy_cords(struct cord *source) {
+    struct cord *head, *curr;
     if (!source) return NULL;
 
-    struct cord *head = malloc(sizeof(struct cord));
-    struct cord *curr = head;
+    head = malloc(sizeof(struct cord));
+    curr = head;
     curr->value = source->value;
     source = source->next;
 
@@ -35,8 +39,7 @@ struct cord* copy_cords(struct cord *source) {
     return head;
 }
 
-
-// Adding the vector value to sum
+/* Add one vector to another */
 void add_vector(struct cord *sum, struct cord *v) {
     while (sum && v) {
         sum->value += v->value;
@@ -45,7 +48,7 @@ void add_vector(struct cord *sum, struct cord *v) {
     }
 }
 
-// Calculating new centroid by dividing the sum of vectors by the number of vectors
+/* Divide vector by a scalar */
 void divide_vector(struct cord *v, int divisor) {
     while (v) {
         v->value /= divisor;
@@ -53,11 +56,11 @@ void divide_vector(struct cord *v, int divisor) {
     }
 }
 
-// Calculating distance between vectors by Euclidean distance
+/* Euclidean distance between vectors */
 double delta_between_vectors(struct cord *a, struct cord *b) {
-    double sum = 0;
+    double sum = 0.0, delta = 0.0;
     while (a && b) {
-        double delta = a->value - b->value;
+        delta = a->value - b->value;
         sum += delta * delta;
         a = a->next;
         b = b->next;
@@ -65,7 +68,7 @@ double delta_between_vectors(struct cord *a, struct cord *b) {
     return sqrt(sum);
 }
 
-// Printing vector values
+/* Print a vector */
 void print_vector(struct cord *cords) {
     while (cords) {
         printf("%.4f", cords->value);
@@ -75,7 +78,7 @@ void print_vector(struct cord *cords) {
     printf("\n");
 }
 
-// Clearing memory starting from any struct head
+/* Free a list of cords */
 void free_cords(struct cord *head) {
     struct cord *temp;
     while (head) {
@@ -84,7 +87,8 @@ void free_cords(struct cord *head) {
         free(temp);
     }
 }
-// Clearing memory starting from any vector head.
+
+/* Free a list of vectors */
 void free_vectors(struct vector *head) {
     struct vector *temp;
     while (head) {
@@ -97,8 +101,18 @@ void free_vectors(struct vector *head) {
 
 int main(int argc, char **argv) {
     int k, iter = 400;
-    int i, n = 0;
+    int i, n = 0, count_iter, stop;
+    int *counts;
+    int min_index;
+    double val, min_dist, delta;
+    char ch;
 
+    struct vector *head_vec, *curr_vec, *vec_iter;
+    struct cord *head_cord, *curr_cord;
+    struct cord **centroids;
+    struct cord **sums;
+
+    /* Input arguments check */
     if (argc != 2 && argc != 3) {
         printf("An Error Has Occurred\n");
         return 1;
@@ -118,16 +132,15 @@ int main(int argc, char **argv) {
         }
     }
 
-    // Process data from given file
-    struct vector *head_vec = malloc(sizeof(struct vector));
-    struct vector *curr_vec = head_vec;
-    struct cord *head_cord = malloc(sizeof(struct cord));
-    struct cord *curr_cord = head_cord;
-    curr_cord->next = NULL;
-    curr_vec->next = NULL; 
+    /* Reading vectors from input */
+    head_vec = malloc(sizeof(struct vector));
+    curr_vec = head_vec;
+    curr_vec->next = NULL;
 
-    double val;
-    char ch;
+    head_cord = malloc(sizeof(struct cord));
+    curr_cord = head_cord;
+    curr_cord->next = NULL;
+
     while (scanf("%lf%c", &val, &ch) == 2) {
         curr_cord->value = val;
         if (ch == '\n') {
@@ -135,6 +148,7 @@ int main(int argc, char **argv) {
             curr_vec->next = malloc(sizeof(struct vector));
             curr_vec = curr_vec->next;
             curr_vec->next = NULL;
+
             head_cord = malloc(sizeof(struct cord));
             curr_cord = head_cord;
             curr_cord->next = NULL;
@@ -151,55 +165,57 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-
-    // Initialized centroids as first k vectors
-    struct vector *vec_iter = head_vec;
-    struct cord **centroids = malloc(k * sizeof(struct cord *));
+    /* Initialize centroids with first k vectors */
+    vec_iter = head_vec;
+    centroids = malloc(k * sizeof(struct cord *));
     for (i = 0; i < k; i++) {
         centroids[i] = copy_cords(vec_iter->cords);
         vec_iter = vec_iter->next;
     }
-    // The k-means algorithm.
 
-    //Creating zeroing vectors for the scheme
-    for (int count_iter = 0; count_iter < iter; count_iter++) {
-        int *counts = calloc(k, sizeof(int));
-        struct cord **sums = malloc(k * sizeof(struct cord *));
+    /* Main K-Means loop */
+    for (count_iter = 0; count_iter < iter; count_iter++) {
+        counts = calloc(k, sizeof(int));
+        sums = malloc(k * sizeof(struct cord *));
         for (i = 0; i < k; i++) {
+            struct cord *curr, *ref;
+
             sums[i] = calloc(1, sizeof(struct cord));
-            struct cord *curr = sums[i];
-            struct cord *ref = centroids[i];
+            curr = sums[i];
+            ref = centroids[i];
             while (ref->next) {
                 curr->next = calloc(1, sizeof(struct cord));
                 curr = curr->next;
                 ref = ref->next;
             }
         }
-        //Calculating the distance between any vector to his nearest centroid
+
+        /* Assign vectors to nearest centroid */
         vec_iter = head_vec;
         while (vec_iter && vec_iter->cords) {
-            double min_dist = INFINITY;
-            int min_index = 0;
+            min_dist = INFINITY;
+            min_index = 0;
+
             for (i = 0; i < k; i++) {
-                double dist = delta_between_vectors(vec_iter->cords, centroids[i]);
-                if (dist < min_dist) {
-                    min_dist = dist;
+                delta = delta_between_vectors(vec_iter->cords, centroids[i]);
+                if (delta < min_dist) {
+                    min_dist = delta;
                     min_index = i;
                 }
             }
-            //update Sum and count after Calculating the nearest centroid
+
             add_vector(sums[min_index], vec_iter->cords);
             counts[min_index]++;
             vec_iter = vec_iter->next;
         }
 
-        int stop = 1;
+        /* Recompute centroids */
+        stop = 1;
         for (i = 0; i < k; i++) {
-            //New centroid calculation
             if (counts[i] != 0)
                 divide_vector(sums[i], counts[i]);
-            
-            double delta = delta_between_vectors(centroids[i], sums[i]);
+
+            delta = delta_between_vectors(centroids[i], sums[i]);
             if (delta >= EPSILON)
                 stop = 0;
 
@@ -210,10 +226,12 @@ int main(int argc, char **argv) {
 
         free(sums);
         free(counts);
+
         if (stop)
             break;
     }
 
+    /* Output centroids */
     for (i = 0; i < k; i++) {
         print_vector(centroids[i]);
         free_cords(centroids[i]);
