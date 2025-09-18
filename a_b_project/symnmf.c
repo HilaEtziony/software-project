480bdb394ad160a811a3ac8137262470dea1a9a4
gadcod <gadrozen5@gmail.com>
2025-09-18 01:54:26 +0000

    a

--- a_b_project/symnmf.c
+++ a_b_project/symnmf.c
@@ -707,7 +707,8 @@ static void run_symnmf_iterations(double **Wmat, double **Hmat,
 void symnmf_symnmf(struct cord **H, struct vector *W) {
     int n_w, m_w, n ,i, k;
     double **Wmat, **Hmat, **WH, **HHt, **HHTH;
-
+    
+    if (H == NULL || H[0] == NULL || W == NULL) return;
     k = cords_len(H[0]);
     Wmat = vectors_to_dense(W, &n_w, &m_w);
     if (n_w == 0 || n_w != m_w) { free_dense(Wmat, n_w); return; }
